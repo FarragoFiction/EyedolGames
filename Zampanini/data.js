ae94ac81230466d3953f2161b889ab7186490e40
JR <jadedResearcher@gmail.com>
2023-01-01 20:56:25 -0500

    icecream

--- Zampanini/data.js
+++ Zampanini/data.js
@@ -1268,14 +1268,14 @@ const initObjects = () => {
 
     object_possibilities[BAKERY] = ["bread","roll","baguette","muffin","slice","pastry"];
     object_possibilities[BREAKFAST] = ["pancakes","bacon","eggs","waffles","toast","french toast","scramble"];
-    object_possibilities[BURGERS ] = ["fry","burger","burger","burger","burger","burger","burger","fries","shake","tots","milkshake"];
+    object_possibilities[BURGERS ] = ["onion rings","cheese","fry","burger","burger","burger","burger","burger","burger","fries","shake","tots","milkshake"];
     object_possibilities[CHICKEN] = ["fry","chicken","chicken","chicken","wing","thigh","chicken breast","tender","fries"];
     object_possibilities[COFFEE ] = ["coffee","coffee","coffee","coffee","coffee","tea","cafe","latte","cappuccino"];
     object_possibilities[DESSERTS ] = ["pie","cake","cupcake","ice cream","custard","frozen yogurt","milkshake"];
     object_possibilities[DINER] = ["casserole","soup","salad","stew","special","burger","fries","potato","wrap","bowl","soup","plate","special"];
     object_possibilities[ITALIAN ] = ["ravioli","pasta","spaghetti","lasagna","parmesan","ziti","penne","noodles","tomato","eggplant"];
     object_possibilities[MEXICAN ] = ["taco","burrito","fajita","arepa","empanada","taquito","quesadilla","salsa","dip"];
-    object_possibilities[PIZZA ] = ["mozarella","pizza","pizza","pizza","pizza","pizza","pizza","calzone","stromboli","sandwich"];
+    object_possibilities[PIZZA ] = ["cheese","mozarella","pizza","pizza","pizza","pizza","pizza","pizza","calzone","stromboli","sandwich"];
     object_possibilities[PREMIUM ] = ["lobster","crab","fresh baked bread","potato","steak","chop"];
     object_possibilities[SALAD ] = ["salad","fry","soup","breadbowl"];
     object_possibilities[SANDWICHES ] = ["wrap","pita","sandwich","hoagie","sub"];
