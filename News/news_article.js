db75ee9a9798f53531b89f392a76fc58dc8ed99c
JR <jadedResearcher@gmail.com>
2023-01-07 19:11:25 -0500

    scroll

--- News/news_article.js
+++ News/news_article.js
@@ -14,7 +14,49 @@ let crimeStatus = MISSING; //certainty is hard to come by
 let road_endings = ["Lane", "Highway", "Avenue", "Drive", "Road", "Crossing", "Street", "Boulevard"];
 
 
+const handleScrolling = (container, rand, existing_keys) => {
+  let lastScrollTime = 0; //not to spam events
+  window.onscroll = () => {
+    const newTime = new Date().getTime();
+    if (((newTime - lastScrollTime)) < 1050) {
+      return;
+    }
+    lastScrollTime = newTime;
+
+    window.requestAnimationFrame(() => {
+      renderOneNewsArticle();
+
+    });
+
+  };
+}
+
+const renderOneNewsArticle = ()=>{
+  //it gets weirder but not like, terribly weirder. its already weird
+  chosen_extra_themes.push(rand.pickFrom(Object.keys(all_themes)));
+  const parent = document.querySelector("body");
+
+  const article = createElementWithClassAndParent("div",parent,"article");
+  article.innerHTML = `
+  <div class="article">
+  <div class="content"><h2 class="headline">Test</h2>
+  <p>In a shocking turn of events, the Baker's Restaurant driver, was in the process of delivering food to Adam Gently, transformed into a monstrous creature and attacked Adam Gently, waiting for a Baker's Restaurant delivery, notably featuring a flavorful Delicious Roll.</p><p>Eye witnesses, describing the Baker's Restaurant driver, say it has a red rubber clown nose. Authorities were not available for comment. Authorities say this is likely some form of hazing by local teens.</p><p>It is currently unclear where Adam Gently or even the Baker's Restaurant driver is. </p><p>When authorities arrived on the scene, the monster had fled the area, but not before causing considerable damage to the exterior of the building. It is currently unknown what the monster was trying to accomplish by its actions or where it has gone.</p><p>Eye witnesses, describing the Baker's Restaurant driver, say it dripped rotten eggs as it walked. Authorities were not available for comment. Authorities say this is likely some form of hazing by local teens.</p></div>
+
+  <a target="blank" href="http://eyedolgames.com/Zampanini?name=Baker%27s+Restaurant&amp;themes=Bakery&amp;feeUnder=21&amp;victim=Adam+G+" class="link3">
+    <img class="left" src="http://eyedolgames.com/Zampanini/images/Bakery/00146-img_20230101202953.png" style="object-fit: none;">
+    <div class="center" style="background-color: rgb(68, 92, 98);"><div class="logo">😸</div><div class="restaurant_name">Baker's Restaurant</div><div class="slogan">Powered by Zampanini. Funny. Every time.</div></div>
+    <img class="right" src="http://eyedolgames.com/Zampanini/images/Bakery/00065-img_20221231212905.png" style="object-fit: none;">
+  </a>
+  <p>
+  </p><div class="more-content"><p>According to witnesses, the Baker's Restaurant driver has not yet been identified suddenly began to grow in size and change in appearance. Within minutes, they had become a terrifying monster, it wore a baker's hat and dragged a long rolling pin behind it embedded with spikes..</p><p>Eye witnesses, describing the assailant, say it wore a baker's hat and dragged a long rolling pin behind it embedded with spikes. Authorities were not available for comment. It is unclear if Adam Gently was on any mind altering substances.</p><p>Eye witnesses, describing the Baker's Restaurant driver, say it wore a baker's hat and dragged a long rolling pin behind it embedded with spikes. Authorities were not available for comment. It is unclear if Adam Gently was on any mind altering substances.</p><p>When authorities arrived on the scene, the monster had fled the area, but not before causing considerable damage to the exterior of the building. It is currently unknown what the monster was trying to accomplish by its actions or where it has gone.</p><p>It is unclear at this time what caused the Baker's Restaurant driver to transform into a monster or how many other potential victims besides Adam Gently there are. The investigation into the incident is ongoing, and authorities are urging residents to be vigilant and to report any unusual activity to the police. Police say that citizens should remain in their homes and they will be safe.</p></div>
+</div>`
+
+  fleshOutNewsArticle(article);
+  setUpMiddleAD(article.querySelector(".link3"))
+
+}
 const initialize_details = () => {
+  handleScrolling();
   restaurant_name = details.restaurant_name;
   how_many_died = rand.getRandomNumberBetween(1, 8);
   customer_was_monster = rand.nextDouble() > 0.75;
@@ -34,7 +76,8 @@ const initialize_details = () => {
   corner1 = `${quick(OBJECT, true)} ${rand.pickFrom(road_endings)}`;
   corner2 = `${quick(PERSON, true)} ${rand.pickFrom(road_endings)}`;
   neighborhood = `${quick(ADJ, true)} ${quick(OBJECT,true)}`;
-
+  const headline = document.querySelector(".headline");
+  headline.innerText = getHeadline();
 
 }
 
@@ -62,11 +105,10 @@ const replaceFirstArticleAndHeader = () => {
 }
 
 const fleshOutNewsArticle = (ele) => {
-  const headline = document.querySelector(".headline");
-  headline.innerText = getHeadline();
+
 
   const innerHeadline = ele.querySelector(".headline");
-  innerHeadline.innerText = headline.textContent;
+  innerHeadline.innerText = getHeadline();
 
   const content = ele.querySelector(".content");
   content.innerText = "";
@@ -81,9 +123,12 @@ const fleshOutNewsArticle = (ele) => {
 }
 
 const getActions = (monster)=>{
-  if(!monster && (victim_name === customer_name)){
+
+
+//i'm llking for the victim and the victim is the customer
+  if(!monster && (victim_name === customer_name) || (monster && victim_name !== customer_name)) {
     return[`a resident of the local neighborhood of ${neighborhood}`,`waiting for a ${restaurant_name} delivery, notably featuring a flavorful ${details.item_name}`];
-  }else{
+  }else{ //i'm looking for the victim and the victim is not the customer
     return  [`has not yet been identified`,`was in the process of delivering food to ${customer_name}`,`on a routine delivery`,`delivering to ${neighborhood}`,`making a ${details?.restaurant_name} delivery to a local residence on ${corner1} last night.`];
   }
 }
@@ -140,9 +185,12 @@ const getMiddle = () => {
     templates.push(`${monster_name},  before their death, reported that the monster ${monster_name} turned into seemed to have no awareness of its actions and appeared to be in a state of frenzy. Authorities arrived on the scene shortly thereafter and were able to subdue the creature, but not before it caused significant damage to the residence.`)
 
   }else if (crimeStatus === MISSING){
-    templates.push(`When authorities arrived on the scene, the monster had fled the area, but not before causing considerable damage to the exterior of the building. It is currently unknown what the monster was trying to accomplish by its actions or where it has gone.`,`Eye witnesses, describing the assailant, say ${quick(MONSTER_DESC)} Authorities were not available for comment. ${rand.pickFrom(hollowReassurances)}`)
-
+    templates.push(`When authorities arrived on the scene, the monster had fled the area, but not before causing considerable damage to the exterior of the building. It is currently unknown what the monster was trying to accomplish by its actions or where it has gone.`)
+    templates.push(`Eye witnesses, describing the assailant, say ${quick(MONSTER_DESC)} Authorities were not available for comment. ${rand.pickFrom(hollowReassurances)}`)
+    templates.push(`It is currently unclear where ${victim_name} or even ${monster_name} is. `)
   }else{
+    templates.push(`${victim_name},  currently hospitalized, is quoted as saying they never even want to see a ${details.item_name} again.`)
+
     templates.push(`${victim_name},  who was able to escape with minor injuries, reported that the monster ${monster_name} had become seemed to have no awareness of its actions and appeared to be in a state of frenzy. Authorities arrived on the scene shortly thereafter and were able to subdue the creature, but not before it caused significant damage to the residence.`)
   }
 
