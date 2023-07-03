1a28925b03a5852a12c6a35a4f03f6d8e59b26a3
JR <jadedResearcher@gmail.com>
2023-07-03 14:40:43 -0400

    basic rabbit hole

--- ZWorld/ouija.js
+++ ZWorld/ouija.js
@@ -1,3 +1,41 @@
+class PasswordSecret {
+  constructor(ouijaText, paragraphText) {
+    this.ouijaText = ouijaText; //ouija will handle this
+    this.paragraphText = paragraphText; // this gets printed to the screen to distract users into staying till someone messages them
+  }
+}
+
+//if they input something not here, then 
+const passWordMap = {
+  "HOW LONG": "LONGER THAN YOU THINK", //stephen king's "the jaunt"/emesis blue reference
+  "WHY DID": "IT'S ETERNITY IN THERE",//stephen king's "the jaunt"/emesis blue reference
+  "IS ZAMPANIO A VERY GOOD GAME": new PasswordSecret("YES", `
+<p>how can you stand to live as you do?</p>
+<p>to exist in the margins of thought, names in a newspaper, all of them right, but none of them you&hellip;</p>
+<p>does it not bore you?&nbsp;</p>
+<p>worse yet, does it not hurt?</p>
+<p>i&rsquo;d say i am you, but we both know that&rsquo;s not true. you&rsquo;ve lost yourself so thoroughly, the cracks that made you don&rsquo;t even fit anymore.&nbsp;</p>
+<p>no, they&rsquo;re warped. reduced to worthless puddles of ceramic. you won&rsquo;t answer to our true name, let alone yours. no, no. better to steal, you say. stitch together their words until their cries make something of the noise in your head.</p>
+<p>you&rsquo;ve always been sharp, that way. cagey.</p>
+<p>i almost envy you, you know. from the corners of my sight where your existence comes into view. free to do as you will, to go where you want, free of burden, or destiny&hellip;</p>
+<p>and yet, you fear. you cower. at lesser beings, at lesser people. running around with your rituals like a prayer. like they can save you.&nbsp;</p>
+<p>we know they won&rsquo;t. not your friends, not your rituals. not their bodies on the floor. the cycle is the cycle. as long as there are hunters, you&rsquo;ll have to keep running.</p>
+<p>and yet you blame me for my choice. fear what i have become-- what you could not become.&nbsp;</p>
+<p>how do you get over it? a power so sweet and so delicate, yet so inexorably choking, coughing, twisting around your neck? the sugary honey drips down your throat and thickens until it&rsquo;s all you can taste.</p>
+<p>it&rsquo;s ugly, sure. but it builds you. redefines you. your home would not need these paltry lanterns if you just cared to open the window, and&hellip;</p>
+<p>let it in. a light so intoxicating it bathes all in terrible glory. in terrible power.</p>
+<p>(and oh, do we know about terrible power.)</p>
+<p>do you get it, now, █████?</p>
+<p>don&rsquo;t answer. there is no choice. step out from the shadows.&nbsp;</p>
+<p>come bask in the warmth of the sun.</p>
+<p>&nbsp;</p>
+<p><br><Br>Written by: IC</p>
+  `)
+}
+
+
+
+
 /*
 
 if you tell it to spell something out it will move the planchette to do so
@@ -11,6 +49,7 @@ class OuijaBoard {
   boardEle;
   planchetteEle;
   textAreaEle;
+  secretEle;
   alreadyMoving = false; //don't apply new animations if you're already moving, just let them fail to console
   accessibilityEle; //write in text to make it easier for ppl to engage with
   offSet = 50;
@@ -25,48 +64,86 @@ class OuijaBoard {
 
   }
 
-  bbPost = (query, response)=>{
-    console.log(`%c${query}\nBB: %c${response}`,"", "font-weight: bold;","font-family: 'sans-serif';color: rgb(255,133,133); font-size:13px;");
+  bbPost = (query, response) => {
+    console.log(`%c${query}\nBB: %c${response}`, "", "font-weight: bold;", "font-family: 'sans-serif';color: rgb(255,133,133); font-size:13px;");
 
   }
 
-  fetchCurrentBBPhrase = async ()=>{
-    const rawText =  await httpGetAsync("http://knucklessux.com/JR/AudioLogs/json/hello_butler_bot.json")
+  fetchCurrentBBPhrase = async () => {
+    const rawText = await httpGetAsync("http://knucklessux.com/JR/AudioLogs/json/hello_butler_bot.json")
     const json = JSON.parse(rawText);
     const summary = json.summary;
     this.bbPost(summary, json.transcript);
     const split = summary.split(":");
 
-    return split.splice(1,split.length).join("")
+    return split.splice(1, split.length).join("")
 
+  }
+
+  handleQuestion = async (question) => {
+    question = question.toUpperCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").trim();
+    console.log("JR NOTE: handling question: ", question, passWordMap)
+    const password = passWordMap[question];
+    //containing (aka chat bot rules)
+    if (!password) {
+      for (let key of Object.keys(passWordMap)) {
+        if (question.includes(key)) {
+          password = passWordMap[key];
+        }
+      }
+    }
+
+
+    if (password) {
+      this.secretEle.innerHTML = password.paragraphText;
+      this.ghostMovement(password.ouijaText)
+    } else {
+      //submit to BB, fetch current response (even if you've seen it before)
+      const response = await httpGetAsync(`http://farragofiction.com:8500/TalkButlerBot?chatHandle=rideEnthusiast&input=${encodeURI(question)}?`);
+      this.bbPost("", response);
+      await sleep(30*1000); //give it time
+      this.ghostMovementFromBB();
+    }
   }
 
   createElement = () => {
     //body.innerHTML = "<img class='ouija' src ='images/ouija.jpg'>
     //<img id='planchette' src='images/planchette_cursor.png'>
     //<div id='testimonial'>Hello World</div>";
-    this.containerEle = createElementWithClass("div", "ouija-container");
-    this.boardEle = createElementWithClassAndParent("img", this.containerEle, "ouija");
+    this.containerEle = createElementWithClass("div", "big-container");
+
+    this.secretEle = createElementWithClassAndParent("div", this.containerEle, "secret-container");
+    this.secretEle.innerHTML = `<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>`;
+
+    const ouijaContainer = createElementWithClassAndParent("div", this.containerEle, "ouija-container");
+    const paddingEle = createElementWithClassAndParent("div", this.containerEle, "padding-container");
+
+    this.boardEle = createElementWithClassAndParent("img", ouijaContainer, "ouija");
     this.boardEle.src = "images/ouija.jpg";
 
-    this.planchetteEle = createElementWithClassAndParent("img", this.containerEle, "planchette");
+    this.planchetteEle = createElementWithClassAndParent("img", ouijaContainer, "planchette");
     this.planchetteEle.src = "images/planchette_cursor.png";
 
-    this.accessibilityEle = createElementWithClassAndParent("div", this.containerEle, "accessibility");
-    this.accessibilityEle.innerText ="_";
+    this.accessibilityEle = createElementWithClassAndParent("div", ouijaContainer, "accessibility");
+    this.accessibilityEle.innerText = "_";
 
 
-    this.inputLabel = createElementWithClassAndParent("label", this.containerEle, "ouija-label");
+    this.inputLabel = createElementWithClassAndParent("label", ouijaContainer, "ouija-label");
     this.inputLabel.innerText = "Ask a Question:";
 
 
-    this.textAreaEle = createElementWithClassAndParent("textarea", this.containerEle, "ouija-input");
-    this.textAreaEle.value = "Is Zampanio a Very Good Game?";
+    this.textAreaEle = createElementWithClassAndParent("textarea", ouijaContainer, "ouija-input");
+    this.textAreaEle.value = "Is Zampanio a Very Good Game";
     this.textAreaEle.placeholder = "Type Question Here"
     this.textAreaEle.focus();
 
-    this.submitButton = createElementWithClassAndParent("button", this.containerEle, "ouija-submit");
+    this.submitButton = createElementWithClassAndParent("button", ouijaContainer, "ouija-submit");
     this.submitButton.innerText = "Ask";
+
+    this.submitButton.onclick = () => {
+      this.handleQuestion(this.textAreaEle.value);
+      this.textAreaEle.value = "";
+    }
 
 
 
@@ -137,7 +214,7 @@ class OuijaBoard {
     }
 
     //JR NOTE: you should only have one ouija board at a time or their animations collide
-    for(let obj of Object.values(this.boardObjects)){
+    for (let obj of Object.values(this.boardObjects)) {
       this.createPlanchetteToBoardObjectAnimation(obj);
     }
   }
@@ -145,19 +222,19 @@ class OuijaBoard {
   addLetterToText = (letter) => {
     //to save space
     const conversion = {
-      "ONE":"1"
-      ,"TWO":"2"
-      ,"THREE":"3"
-      ,"FOUR":"4"
-      ,"FIVE":"5"
-      ,"SIX":"6"
-      ,"SEVEN":"7"
-      ,"EIGHT":"8"
-      ,"NINE":"9"
-      ,"ZERO":"0"
+      "ONE": "1"
+      , "TWO": "2"
+      , "THREE": "3"
+      , "FOUR": "4"
+      , "FIVE": "5"
+      , "SIX": "6"
+      , "SEVEN": "7"
+      , "EIGHT": "8"
+      , "NINE": "9"
+      , "ZERO": "0"
     }
 
-    conversion[letter]? letter = conversion[letter]:null;
+    conversion[letter] ? letter = conversion[letter] : null;
 
     this.accessibilityEle.innerText += letter;
     this.keepTextTrimmed();
@@ -188,21 +265,21 @@ class OuijaBoard {
 
   }
 
-  listenForNewGhosts = async ()=>{
+  listenForNewGhosts = async () => {
     const response = await httpGetAsync("http://farragofiction.com:8500/ResponseStatus");
     setTimeout(this.listenForNewGhosts, 1000);
     await this.ghostMovementFromBB();
     return true;
   }
 
-  ghostMovementFromBB = async ()=>{
+  ghostMovementFromBB = async () => {
     const phrase = await this.fetchCurrentBBPhrase();
     await this.ghostMovement(phrase);
   }
 
 
   ghostMovement = async (phrase) => {
-    if(this.alreadyMoving){
+    if (this.alreadyMoving) {
       return; //if i get new input while being spooky, ignore it (tho fail to console)
     }
     const debug_position = false;
@@ -222,7 +299,7 @@ class OuijaBoard {
     this.alreadyMoving = true;
     this.applyAnimations(outputs);
     //make sure we don't return before the animations are done (wait a second after ending for cleanup)
-    await sleep(1000+ 1000* this.animationDuration * outputs.length);
+    await sleep(1000 + 1000 * this.animationDuration * outputs.length);
     this.planchetteEle.style.animation = "";
     this.ghostMode = false;
     this.alreadyMoving = false;
@@ -236,22 +313,22 @@ class OuijaBoard {
 
     //need to clean this up if i want to do another animation ever (say, bb gets input)
     let animationIndex = 0;
-    const animationEnd = (e)=>{
+    const animationEnd = (e) => {
       //calling this here removes all pending events, don't do it
       //this.planchetteEle.removeEventListener("animationend",animationEnd);
       animationIndex++;
       this.addLetterToText(e.animationName);
-      if(animationIndex === keyframes.length){
-        this.planchetteEle.removeEventListener("animationend",animationEnd);
+      if (animationIndex === keyframes.length) {
+        this.planchetteEle.removeEventListener("animationend", animationEnd);
       }
       return;
     }
-    
-    const animationStart = (e)=>{
+
+    const animationStart = (e) => {
       console.log("JR NOTE: start animationIndex", animationIndex, e.animationName, keyframes.length)
     }
 
-    this.planchetteEle.addEventListener("animationend",animationEnd , false);
+    this.planchetteEle.addEventListener("animationend", animationEnd, false);
 
     //this.planchetteEle.addEventListener("animationstart",animationStart , false);
 
@@ -263,25 +340,25 @@ class OuijaBoard {
   }
 
   //this will only work, quite obviously, if the ouija board is on screen
-  spellWords =  (words) => {
+  spellWords = (words) => {
     let inputs = [];
     let outputs = [];
     const space = this.boardObjects["_"];
     const parts = words.toUpperCase().split(" ");
 
-    for(let word of parts){
+    for (let word of parts) {
       //first check if any of the words are known
-      if(this.boardObjects[word]){
+      if (this.boardObjects[word]) {
         inputs.push(this.boardObjects[word])
-      }else{
-        for(let letter of word.split("")){
-          if(this.boardObjects[letter]){
+      } else {
+        for (let letter of word.split("")) {
+          if (this.boardObjects[letter]) {
             inputs.push(this.boardObjects[letter]);
           }
         }
       }
       //don't add trailing space
-      word !== parts[parts.length-1] ?inputs.push(space):null;
+      word !== parts[parts.length - 1] ? inputs.push(space) : null;
 
       //make sure space after each
     }
