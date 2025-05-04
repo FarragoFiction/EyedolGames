8ca171b890a900b1529c135429901992233e188c
Jennifer Reynolds <jadedResearcher@gmail.com>
2025-05-04 11:42:13 -0400

    fixed

--- Eyedlr/fun_and_good_ads.js
+++ Eyedlr/fun_and_good_ads.js
@@ -14,13 +14,21 @@ let weird_blurbs;
 const gif_url = "http://farragofiction.com/CatalystsBathroomSim/audio_utils/weird_sounds/weird_video/WeirdGifs/";
 const blurb_url = `http://farragofiction.com/CatalystsBathroomSim/audio_utils/weird_sounds/weird_video/WeirdGifs/WeirdTextBlurbs/`;
 
-
+//gotta have my own internal one cuz im loading weird (like im a stand alone injected script)
+//look i was exhausted from kittens so i did a bit o fspahetti coding
+const isItFridayAds = ()=>{
+  //midnight and fridays are wungle time
+  const date = new Date();
+  if (date.getHours() == 0 || date.getDay() === 5) {
+    return true;
+  }
+  return false;
+}
 
 initSimpleAd = async () => {
-  console.warn("JR NOTE: don't forget to remove this from landing bathroom, its just there as a test")
   const body = document.querySelector("body");
   const simpleAd = createElementWithClassAndParent("div", body, 'simple-ad');
-  simpleAd.style.cursor="pointer"
+  simpleAd.style.cursor = "pointer"
 
   const container = createElementWithClassAndParent("div", simpleAd, 'post-container');
 
@@ -30,36 +38,37 @@ initSimpleAd = async () => {
 
   const postBody = createElementWithClassAndParent("div", container, "post-body");
   const postTitle = createElementWithClassAndParent("div", postBody,);
-  postTitle.style.cssText =`font-weight: 600;
+  postTitle.style.cssText = `font-weight: 600;
     opacity: 0.6;
     font-size: 20px;
     text-transform: capitalize;`;
 
   weird_blurbs = await getWeirdTextBlurbSimpleAd(blurb_url);
-  postTitle.innerHTML = await blurbFromNetwork(blurb_url +pickFrom(weird_blurbs))
+  postTitle.innerHTML = await blurbFromNetwork(blurb_url + pickFrom(weird_blurbs))
 
   weird_gifs = await getWeirdImageSimpleAd(gif_url);
   const gif = createElementWithClassAndParent("img", postBody);
   gif.src = gif_url + pickFrom(weird_gifs);
-  gif.style.width ="222px";
+  gif.style.width = "222px";
 
   const postContent = createElementWithClassAndParent("div", postBody,);
-  postContent.innerHTML = await blurbFromNetwork(blurb_url +pickFrom(weird_blurbs))
-  postContent.style.cssText =`font-weight: 300;
+  postContent.innerHTML = await blurbFromNetwork(blurb_url + pickFrom(weird_blurbs))
+  postContent.style.cssText = `font-weight: 300;
     opacity: 0.6;
     font-size: 12px;`;
 
-    if(isItFriday()){
-      postTitle.innerHTML = "You Should Rest"
-      postContent.innerHTML = "Zampanio Will Still Be Here Later. Friday's and the Midnight Hour are our reminders that Zampanio needs us to live a long, fullfilling, varied life."
-      gif.src = "http://eyedolgames.com/News/images/Quizzes/jr_modified.gif";
-    }else{
-      simpleAd.onclick = async()=>{
-        postTitle.innerHTML = await blurbFromNetwork(blurb_url +pickFrom(weird_blurbs))
-        postContent.innerHTML = await blurbFromNetwork(blurb_url +pickFrom(weird_blurbs))
-        gif.src = gif_url + pickFrom(weird_gifs);
-      }
+
+  if (isItFridayAds()) {
+    postTitle.innerHTML = "You Should Rest"
+    postContent.innerHTML = "Zampanio Will Still Be Here Later. Friday and the Midnight Hour are our reminders that Zampanio needs us to live a long, fullfilling, varied life."
+    gif.src = "http://eyedolgames.com/News/images/Quizzes/jr_modified.gif";
+  } else {
+    simpleAd.onclick = async () => {
+      postTitle.innerHTML = await blurbFromNetwork(blurb_url + pickFrom(weird_blurbs))
+      postContent.innerHTML = await blurbFromNetwork(blurb_url + pickFrom(weird_blurbs))
+      gif.src = gif_url + pickFrom(weird_gifs);
     }
+  }
 
 
 
@@ -177,7 +186,7 @@ const getWeirdTextBlurbSimpleAd = async (url) => {
 
 
 
-if(Math.random()>0.05){ //just to keep you guessing if this really exists
+if (Math.random() > 0.05) { //just to keep you guessing if this really exists
   console.log("JR NOTE: its time to parody tumblr ads baby")
   window.addEventListener("load", initSimpleAd)
 }
